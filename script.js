22f5cb2ce9a42eb4e9a04cb7e40078c3ea5c8206
usmanhaider461 <46385291+usmanhaider461@users.noreply.github.com>
2020-12-17 12:59:09 +0500

    Random Color Generator

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var message = document.getElementById("message");
+var h1 = document.querySelector("h1");
 colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++) {
@@ -15,6 +16,7 @@ for(var i = 0; i < squares.length; i++) {
     var clickedColor = this.style.backgroundColor;
     if(clickedColor === pickedColor) {
         changeColors(clickedColor);
+        h1.style.backgroundColor = clickedColor;
         message.textContent = "Correct!"
     } else {
         this.style.backgroundColor = "#232323";
@@ -38,5 +40,24 @@ function pickColor(){
 }
 
 function generateRandomColors(numberOfColors) {
-    //
+    //make an array
+    // add random colors to array
+    // return that array
+    var arr = [];
+    // loop numberOfColors times
+    for(var i = 0; i < numberOfColors; i++) {
+        // get random colors and push into array
+        arr.push(randomColor());    
+    }
+    return arr;
 }
+
+function randomColor() {
+    // pick a "red" from 0 - 255
+   var r = Math.floor(Math.random() * 256);
+    // pick a "green" from 0 - 255
+   var g = Math.floor(Math.random() * 256);
+    // pick a "blue" from 0 - 255
+   var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+  }
